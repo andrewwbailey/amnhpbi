1fa6b1fed5cb2d46e9f9e2afa3008a85baea329f
Andrew Bailey <him@andrewwarrenbailey.com>
2012-08-03 16:45:53 -0400

    CSS, beginning of pagination, thinking about structure

--- index.php
+++ index.php
@@ -1,22 +1,42 @@
 ﻿<?php
-$page_title = 'Planetary Biodiversity Inventory';
+$page_title = 'PBI - AMNH';
 include ('inc/pbi_head.html');
-echo '<h1>Planetary Biodiversity Inventory</h1><br />';
+
+echo "<header>
+	<h1>Planetary Biodiversity Inventory for Plant Bugs</h1> <br />
+	</header>";
+
 
 require_once("../../../connect_pbilocality.php");
+
+$display = 200;
+
+$stmtCount = $mysqli->prepare("SELECT COUNT(localityuid) FROM Locality");
+
+if (!$stmtCount->execute()) {
+	echo "Database query error: (" . $stmtCount->errno . ") " . $stmtCount->error;
+	}
+
+$stmtCount->bind_result($recCount);
+
+while($stmtCount->fetch()){
+	echo $recCount . "<p/>";
+}
+$stmtCount->close();
+
+$pages = ceil($recCount/$display);
+
+echo "Pages: " . $pages;
+
 // Prepare the query:
-$stmt = $mysqli->prepare("SELECT localityuid, localitystr, dlat, dlong, nnotes, sitename, createdate, updatedate, createdby, updatedby FROM Locality LIMIT 50");
+$stmtMain = $mysqli->prepare("SELECT localityuid, localitystr, dlat, dlong, nnotes, sitename, createdate, updatedate, createdby, updatedby FROM Locality LIMIT 200");
 
-if (!$stmt->execute())
-  echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-
-$stmt->store_result();
-$stmt->bind_result($col1,$col2,$col3,$col4,$col5,$col6,$col7,$col8,$col9,$col10);
-
-
-echo "Number of rows: " . $stmt->num_rows . "<br />";
-echo "Number of fields: " . $stmt->field_count . "<br />";
+if (!$stmtMain->execute()) {
+	echo "Execute failed: (" . $stmtMain->errno . ") " . $stmtMain->error;
+	}
   
+$stmtMain->bind_result($col1,$col2,$col3,$col4,$col5,$col6,$col7,$col8,$col9,$col10);
+
 echo '<table align="center" cellspacing="0" cellpadding="5" width="90%">
 <tr>
 	<td align="left" width="35"><b>Locality ID</b></td>
@@ -31,7 +51,7 @@ echo '<table align="center" cellspacing="0" cellpadding="5" width="90%">
 	<td align="left" width="35"><b>Updated By</b></td>
 </tr>';
 
-while ($stmt->fetch()) {
+while ($stmtMain->fetch()) {
 	echo '<tr>
 		<td align="left">' . $col1 . '</td> 
 		<td align="left">' . $col2 . '</td>
@@ -46,11 +66,7 @@ while ($stmt->fetch()) {
 	</tr>';
 }
 
-$stmt->close();
-$stmt->free_result();
-	
-//mysqli_free_result ($r);
-//mysqli_close($dbc);
+$stmtMain->close();
 
 include ('inc/pbi_foot.html');
 ?>
